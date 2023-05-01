a5c35b6b774afe93a54615487dd19a797440a263
xfilteredwaterx <shaina.milde1@gmail.com>
2023-05-01 23:12:44 +0200

    mac build

---
+++ LightProjectGame/Assets/MyShortcuts.cs
@@ -0,0 +1,14 @@
+﻿#if UNITY_EDITOR
+using UnityEditor;
+using UnityEngine;
+
+public class MyShortcuts : Editor
+{
+    [MenuItem("GameObject/RotateRandom")]
+    static void ToggleActivationSelection()
+    {
+        var go = Selection.activeGameObject;
+        go.transform.eulerAngles = new Vector3(go.transform.eulerAngles.x, Random.Range(0, 360), go.transform.eulerAngles.z);
+    }
+}
+#endif

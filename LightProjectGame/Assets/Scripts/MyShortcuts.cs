3f1779500becb15b2ec33613e7a528998619b1cf
trytomatie <trytomatie@gmail.com>
2023-05-07 16:02:00 +0200

    Random Scale For Shortcuts

--- LightProjectGame/Assets/Scripts/MyShortcuts.cs
+++ LightProjectGame/Assets/Scripts/MyShortcuts.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class MyShortcuts : Editor
 {
+    public static float minScale = 0.8f;
+    public static float maxScale = 1.2f;
     [MenuItem("GameObject/RotateRandom")]
     static void ToggleActivationSelection()
     {
         var go = Selection.activeGameObject;
         go.transform.eulerAngles = new Vector3(go.transform.eulerAngles.x, Random.Range(0, 360), go.transform.eulerAngles.z);
+        float rnd = Random.Range(minScale, maxScale);
+        go.transform.localScale = new Vector3(rnd, rnd, rnd);
     }
 }
 #endif

6bee78e052dd6dae6978a63de553ff130319f1cf
trytomatie <trytomatie@gmail.com>
2023-05-07 16:12:05 +0200

    Update Shortcut

--- LightProjectGame/Assets/Scripts/MyShortcuts.cs
+++ LightProjectGame/Assets/Scripts/MyShortcuts.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class MyShortcuts : Editor
 {
-    public static float minScale = 0.8f;
-    public static float maxScale = 1.2f;
+    public static float minScale = -0.1f;
+    public static float maxScale = 0.1f;
     [MenuItem("GameObject/RotateRandom")]
     static void ToggleActivationSelection()
     {
-        var go = Selection.activeGameObject;
-        go.transform.eulerAngles = new Vector3(go.transform.eulerAngles.x, Random.Range(0, 360), go.transform.eulerAngles.z);
-        float rnd = Random.Range(minScale, maxScale);
-        go.transform.localScale = new Vector3(rnd, rnd, rnd);
+        foreach(GameObject go in Selection.objects)
+        {
+            go.transform.eulerAngles = new Vector3(go.transform.eulerAngles.x, Random.Range(0, 360), go.transform.eulerAngles.z);
+            float rnd = Random.Range(minScale, maxScale);
+            go.transform.localScale = go.transform.localScale + new Vector3(rnd, rnd, rnd);
+        }
+        //var go = Selection.activeGameObject;
+
     }
 }
 #endif

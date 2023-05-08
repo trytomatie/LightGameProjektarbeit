4a20af27b594fcb79a88f5bc879f75495485b401
trytomatie <trytomatie@gmail.com>
2023-05-08 13:54:29 +0200

    Editor Scripts, Physics Placement

--- LightProjectGame/Assets/Scripts/MyShortcuts.cs
+++ LightProjectGame/Assets/Scripts/MyShortcuts.cs
@@ -1,4 +1,5 @@
 ﻿#if UNITY_EDITOR
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
 
@@ -6,10 +7,20 @@ public class MyShortcuts : Editor
 {
     public static float minScale = -0.5f;
     public static float maxScale = 0.5f;
+
+    private static Rigidbody[] allRigidbodies = Object.FindObjectsOfType<Rigidbody>();
+    private static List<Rigidbody> notKinematicRb = new List<Rigidbody>();
+    private static List<Rigidbody> removeRB = new List<Rigidbody>();
+
+    private static Event myEvent;
+
+    private static bool toggleState = false;
+
     [MenuItem("GameObject/RotateRandom")]
     static void ToggleActivationSelection()
     {
-        foreach(GameObject go in Selection.objects)
+        RecordUndoState();
+        foreach (GameObject go in Selection.objects)
         {
             go.transform.eulerAngles = new Vector3(go.transform.eulerAngles.x, Random.Range(0, 360), go.transform.eulerAngles.z);
             float rnd = Random.Range(minScale, maxScale);
@@ -17,6 +28,89 @@ public class MyShortcuts : Editor
         }
         //var go = Selection.activeGameObject;
 
+    }
+
+    private static void RecordUndoState()
+    {
+        Transform[] transforms = new Transform[Selection.objects.Length];
+        for (int i = 0; i < transforms.Length; i++)
+        {
+            transforms[i] = ((GameObject)Selection.objects[i]).transform;
+        }
+        Undo.RecordObjects(transforms, "Making changes to object");
+    }
+
+    [MenuItem("GameObject/SimulatePhysics")]
+    static void SimulatePhysics()
+    {
+        toggleState = !toggleState;
+        if (toggleState)
+        {
+            RecordUndoState();
+            allRigidbodies = Object.FindObjectsOfType<Rigidbody>();
+            Physics.autoSimulation = false; // Disable automatic physics simulation
+            foreach (Rigidbody rigidbody in allRigidbodies)
+            {
+                if (!rigidbody.isKinematic)
+                {
+                    rigidbody.isKinematic = true;
+                    notKinematicRb.Add(rigidbody);
+                }
+
+            }
+
+            foreach (GameObject go in Selection.objects)
+            {
+                if (go.GetComponent<Rigidbody>() == null)
+                {
+                    removeRB.Add(go.AddComponent<Rigidbody>());
+                }
+                go.GetComponent<Rigidbody>().isKinematic = false;
+            }
+            EditorApplication.update += SimulatePhysicsObjects;
+            Undo.undoRedoPerformed += FixPhysics;
+            myEvent = Event.current;
+        }
+        else
+        {
+            Physics.autoSimulation = true;
+            foreach (Rigidbody rigidbody in notKinematicRb)
+            {
+                rigidbody.isKinematic = false;
+            }
+            for (int i = 0; i < removeRB.Count; i++)
+            {
+                DestroyImmediate(removeRB[i]);
+            }
+            EditorApplication.update -= SimulatePhysicsObjects;
+            Undo.undoRedoPerformed -= FixPhysics;
+        }
+
+    }
+
+    [RuntimeInitializeOnLoadMethod]
+    private static void FixPhysics()
+    {
+        if(toggleState)
+        {
+            Physics.autoSimulation = true;
+            foreach (Rigidbody rigidbody in notKinematicRb)
+            {
+                rigidbody.isKinematic = false;
+            }
+            for (int i = 0; i < removeRB.Count; i++)
+            {
+                DestroyImmediate(removeRB[i]);
+            }
+            EditorApplication.update -= SimulatePhysicsObjects;
+            toggleState = !toggleState;
+        }
+    }
+
+    private static void SimulatePhysicsObjects()
+    {
+
+        Physics.Simulate(Time.fixedDeltaTime); // Step the physics simulation by the fixed time step
     }
 }
 #endif

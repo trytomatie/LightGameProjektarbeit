78f0cd530c774934fc0191704bd81c55de0a5792
trytomatie <trytomatie@gmail.com>
2023-06-28 00:24:30 +0200

    Weapon for Arin, VFX Draggable,  VFX Package implemented, Bug Fix for Light Flicker

--- LightProjectGame/Assets/Scripts/Auras/LightBlight.cs
+++ LightProjectGame/Assets/Scripts/Auras/LightBlight.cs
@@ -7,22 +7,28 @@ namespace Assets.Scripts.Auras
     public class LightBlight : Buff
     {
         private LightController lc;
+        private StatusManager originStatus;
         public LightBlight(StatusManager origin)
         {
             buffname = BuffName.LightBlight;
             duration = 100;
             originID = origin.gameObject.GetInstanceID();
+            originStatus = origin;
         }
         public override void BuffApplication(StatusManager soruce)
         {
             this.target = soruce;
+            
             lc = target.GetComponentInChildren<LightController>();
             lc.Flicker = true;
         }
 
         public override void BuffEffect()
         {
-
+            if(originStatus.Hp <= 0)
+            {
+                duration = 0;
+            }
         }
 
         public override void BuffEnd()
---
+++ LightProjectGame/Assets/VFXGraphHandler.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.VFX;
+
+public class VFXGraphHandler : MonoBehaviour
+{
+    public float duration = 5;
+    internal VisualEffect vfxGraph;
+    public UnityEvent followUpEvent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        vfxGraph = GetComponent<VisualEffect>();
+    }
+
+    public void Play()
+    {
+        vfxGraph.Play();
+        CancelInvoke();
+        Invoke("Stop", duration);
+        followUpEvent.Invoke();
+    }
+    public void PlayDelayed(float delay)
+    {
+        if(vfxGraph == null)
+        {
+            vfxGraph = GetComponent<VisualEffect>();
+        }
+        Invoke("Play", delay);
+    }
+
+    
+
+    public void Stop()
+    {
+        vfxGraph.Stop();
+    }
+
+    public void FollowUpEvent()
+    {
+        followUpEvent.Invoke();
+    }
+}
+
+[CustomEditor(typeof(VFXGraphHandler))]
+public class VFXGraphHandlerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        VFXGraphHandler vfxGraphHandler = (VFXGraphHandler)target;
+        vfxGraphHandler.vfxGraph = vfxGraphHandler.GetComponent<VisualEffect>();
+        if (GUILayout.Button("Play Effect"))
+        {
+            vfxGraphHandler.Play();
+        }
+    }
+}

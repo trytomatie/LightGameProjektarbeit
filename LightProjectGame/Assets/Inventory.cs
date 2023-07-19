77637854f689f09165e5089b196f8174223fe99a
trytomatie <trytomatie@gmail.com>
2023-07-19 19:23:24 +0200

    Inventory

---
+++ LightProjectGame/Assets/Inventory.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private int potionCount;
+
+    public int PotionCount { get => potionCount; set => potionCount = value; }
+}
